d8d17eb197545233b0720a0c33289154789171e2
OliverOliveira <filomenoolivetree@gmail.com>
2025-03-24 21:08:02 +0100

    instalation and configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-import { useFetchPosts } from "@/useCases/Post/useFetchPosts";
+import { Button } from '@/components/ui/button'
+import { useFetchPosts } from '@/useCases/Post/useFetchPosts'
 
 export default async function Home() {
-  const posts = await useFetchPosts();
+  //const posts = await useFetchPosts();
   return (
-    <div>
-      <ul>
+    <div className='p-0.5 flex flex-col justify-center items-center h-screen gap-2'>
+      {/* <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title} - by {post.user.name}</li>
         ))}
-      </ul>
+      </ul> */}
+      <h1>Home</h1>
+      <Button>Click-me!</Button>
     </div>
-  );
+  )
 }
